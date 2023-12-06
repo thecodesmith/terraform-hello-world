1aad8f66a565af07b11e705788762e194bc60291
Brian Stewart <brian.stewart@jamf.com>
2023-12-06 13:47:56 -0600

    Create multi-region KMS CMK and replica key

--- main.tf
+++ main.tf
@@ -14,3 +14,36 @@ module "terraform_state_backend" {
   terraform_backend_config_file_name = "backend.tf"
   force_destroy                      = false
 }
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# US East (N. Virginia) region
+provider "aws" {
+  region = "us-east-1"
+}
+
+# Europe (Frankfurt) region
+provider "aws" {
+  region = "eu-central-1"
+  alias  = "secondary"
+}
+
+resource "aws_kms_key" "primary" {
+  description         = "Primary US region CMK for encrypting CloudWatch logs"
+  multi_region        = true
+  enable_key_rotation = true
+}
+
+resource "aws_kms_replica_key" "replica" {
+  provider                = aws.secondary
+  description             = "EU multi-region replica key for encrypting CloudWatch logs"
+  primary_key_arn         = aws_kms_key.primary.arn
+  deletion_window_in_days = 7
+}
