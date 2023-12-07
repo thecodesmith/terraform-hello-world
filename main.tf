9636bb47f41936a41e8d0daedd190730aed9b6c0
Brian Stewart <brian.stewart@jamf.com>
2023-12-07 17:01:38 -0600

    Fix formatting

--- main.tf
+++ main.tf
@@ -25,12 +25,12 @@ terraform {
 }
 
 variable "primary_region" {
-  type = string
+  type    = string
   default = "us-east-1"
 }
 
 variable "secondary_region" {
-  type = string
+  type    = string
   default = "eu-central-1"
 }
 
@@ -220,9 +220,9 @@ resource "aws_ecr_lifecycle_policy" "main" {
 }
 
 resource "aws_cloudwatch_log_group" "hello_world" {
-  name = "hello-world"
+  name              = "hello-world"
   retention_in_days = 7
-  kms_key_id = aws_kms_key.primary.arn
+  kms_key_id        = aws_kms_key.primary.arn
 }
 
 resource "aws_ecs_cluster" "main" {
@@ -249,8 +249,8 @@ resource "aws_ecs_task_definition" "hello_world" {
     logConfiguration = {
       logDriver = "awslogs"
       options = {
-        awslogs-group = aws_cloudwatch_log_group.hello_world.name
-        awslogs-region = var.primary_region
+        awslogs-group         = aws_cloudwatch_log_group.hello_world.name
+        awslogs-region        = var.primary_region
         awslogs-stream-prefix = "ecs"
       }
     }
